38f103e46561d359318daa96a4c16fd55ddce93c
Kymesa <Kymesa@unicesar.edu.co>
2023-11-18 01:52:40 -0500

    ...

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,23 +1,23 @@
 import "../App.css";
 import Iframe from "react-iframe";
 import { Spinner } from "@material-tailwind/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Header } from "../components/Header";
 import { ActiveDrawer } from "../components/ActiveDrawer";
 import { FooterCalc } from "../components/FooterCalc";
-import { AlertDev } from "../components/AlertDev";
+// import { AlertDev } from "../components/AlertDev";
 const Inicio = (): JSX.Element => {
   const [loading, setLoading] = useState<boolean>(true);
-  const [alertDev, setAlertDev] = useState<boolean>(false);
+  // const [alertDev, setAlertDev] = useState<boolean>(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setAlertDev(true);
-      setTimeout(() => {
-        setAlertDev(false);
-      }, 6000);
-    }, 1500);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setAlertDev(true);
+  //     setTimeout(() => {
+  //       setAlertDev(false);
+  //     }, 6000);
+  //   }, 1500);
+  // }, []);
 
   return (
     <>
@@ -28,7 +28,7 @@ const Inicio = (): JSX.Element => {
           <Spinner className="h-16 w-16 text-gray-900/50" color="blue" />
         </div>
       )}
-      {alertDev && <AlertDev alertDev={alertDev} setAlertDev={setAlertDev} />}
+      {/* {alertDev && <AlertDev alertDev={alertDev} setAlertDev={setAlertDev} />} */}
       <Iframe
         className=" text-center px-2"
         url="https://kymesa.github.io/convert-unit-upc/"
